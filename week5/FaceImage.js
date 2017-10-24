38ad4306272cf6f752fbe437e79a1b5370578a3f
LinYunWen <l40303k@gmail.com>
2017-10-24 21:17:50 +0800

    Bug Fix: Fix relative address

--- week5/FaceImage.js
+++ week5/FaceImage.js
@@ -3,7 +3,7 @@ function setFaceImage() {
     for (let i=0 ; i<9 ; i++){
         base = Math.floor(Math.random()*3 + 1)*10 + Math.floor(Math.random()*4 + 1);
         // $("img.question")[i].attr("src", "/image/face/" +  base.toString() + ".jpg");
-        $("img.question")[i].src = "../week5/image/face/" +  base.toString() + ".jpg";
+        $("img.question")[i].src = "week5/image/face/" +  base.toString() + ".jpg";
     }
 }
 
@@ -36,22 +36,67 @@ function getImageNum(src) {
     return parseInt(src.substring(src.lastIndexOf("/") + 1, src.lastIndexOf(".")));
 }
 
+function getCorrectNum() {
+  let imageId = getImage($("img.question"));
+  let correctNum = 0;
+  for (let i=0 ; i<imageId.length ; i++) {
+      if ((imageId[i] % 2) == 0) {
+          correctNum++;
+      }
+  }
+  return correctNum;
+}
+
+function showAnswers() {
+  let images = $("img.question")
+  let imagesId = getImage(images);
+
+  for (let i=0 ; i<images.length ; i++) {
+      if ((imagesId[i] % 2) == 0) {
+          console.log("correct: ", i);
+          markCorrect(images[i]);
+      } else {
+          console.log("error: ", i);
+          markFalse(images[i]);
+      }
+  }
+}
+
+function markCorrect(image) {
+    if (!image.classList.contains("selected")) {
+        console.log("set green: ", image);
+        image.style.borderColor = "rgba(0, 255, 0, 1)";
+    }
+}
+
+function markFalse(image) {
+    if (image.classList.contains("selected")) {
+        console.log("set red: ", image);
+        image.style.borderColor = "rgba(255, 0, 0, 1)";
+    }
+}
+
 function clickFaceSubmit(event) {
     event.preventDefault();
     let select = getImage($("img.selected"));
     console.log("select: ", select);
-    /*
+
+    let correctNum = getCorrectNum();
+    ///*
     $.ajax({
         data: {
             selectedImages: select
         },
-        url: "recognizer.php",
+        url: "week5/recognizer.php",
         success: function(data) {
             console.log("success");
             console.log("data: ", data);
+            console.log("correct num: ", correctNum);
+            $("div#result")[0].textContent = "result: " + (data*100/correctNum).toString() + "分";
+            showAnswers();
         }
     });
-    */
+    //*/
     
 }
 
